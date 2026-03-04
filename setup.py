cbdb1d24999a057f2cc6c6e81a0fd3e84eb6a695
Ivan Savov <ivan.savov@gmail.com>
2026-03-04 13:37:49 -0500

    Bump version: 0.5.6 → 0.5.7

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/minireference/ministats',
-    version='0.5.6',
+    version='0.5.7',
     zip_safe=False,
 )
