f5e57f49115fb9e1e8df0e5fb78920b475e24d1f
Ivan Savov <ivan.savov@gmail.com>
2026-04-22 13:54:04 -0400

    Bump version: 0.5.9 → 0.5.10

--- ministats/__init__.py
+++ ministats/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Ivan Savov"""
 __email__ = 'ivan@minireference.com'
-__version__ = '0.5.9'
+__version__ = '0.5.10'
 
 
 from .bayes import (
--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/minireference/ministats',
-    version='0.5.9',
+    version='0.5.10',
     zip_safe=False,
 )
